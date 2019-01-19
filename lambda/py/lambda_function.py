f8c54d5d4393806530b93ac3bd6d7d916896df15
Alex Zheng <huiyaozheng@users.noreply.github.com>
2019-01-19 23:18:37 +0000

    Update lambda_function.py

--- lambda/py/lambda_function.py
+++ lambda/py/lambda_function.py
@@ -47,6 +47,49 @@ data = [
   'The Moon is moving approximately 3.8 cm away from our planet every year.',
 ]
 
+narration = {
+    "start" : "Everyone, close your eyes.";
+    "wolves_1" : "Werewolves, wake up and look for other werewolves. If there is only one werewolf, you may look at a card from the centre.";
+    "wolves_2" : "Werewolves, close your eyes.";
+    "mystic_wolf_1" : "Mystic wolf, if you did not look at a card from the centre just now, you can wake up and do so.";
+    "mystic_wolf_2" : "Mystic wolf, close your eyes.";
+    "minion_1" : "Minion, wake up. Werewolves, stick out your thumb so the minion can see who you are.";
+    "minion_2" : "Werewolves, put your thumb away. Minion, close your eyes.";
+    "seer_1" : "Seer, wake up. You may look at another player's card or two of the centre cards.";
+    "seer_2" : "Seer, close your eyes.";
+    "apprentice_seer_1" : "Apprentice seer, wake up. You may look at one of the centre cards.";
+    "apprentice_seer_2" : "Apprentice seer, close your eyes.";
+    "pi_1" : "Paranormal investigator, wake up. You may look at up to two cards of other players. However, if you see a wolf, you become a wolf for this game and stop your action.";
+    "pi_2" : "Paranormal investigator, close your eyes.";
+    "robber_1" : "Robber, wake up. You must exchange your card with another player's card, and then view your new card.";
+    "robber_2" : "Robber, close your eyes.";
+    "troublemaker_1" : "Troublemaker, wake up. You may exchange cards between two other players.";
+    "troublemake_2" : "Troublemaker, close your eyes.";
+    "village_idiot_1" : "Village idiot, wake up. You may move everyone's card except your own to the left or to the right.";
+    "village_idiot_2" : "Village idiot, close your eyes.";
+    "drunk_1" : "Drunk, wake up and exchange your card with a card in the centre.";
+    "drunk_2" : "Drunk, close your eyes.";
+    "insomniac_1" : "Insomniac, wake up and have a look at your card.";
+    "insomniac_2" : "Insomniac, close your eyes.";
+    "end" : "Everyone, keep your eyes closed and reach out and move your card around slightly. Everyone, wake up!"
+}
+
+card_definition = {
+    "wolves" : "black aces";
+    "mystic wolf" : "ace of spade";
+    "minion" : "a black two";
+    "seer" : "a red seven";
+    "apprentice seer" : "a red five";
+    "paranormal investigator" : "joker";
+    "robber" : "a red jack";
+    "trouble maker" : "a red queen";
+    "drunk" : "a red ten";
+    "insomniac" : "a red nine";
+    "tanner" : "a black four";
+    "village idiot" : "a red five";
+    "villagers" : "red threes"
+}
+
 # =========================================================================================================================================
 # Editing anything below this line might break your skill.
 # =========================================================================================================================================
@@ -67,6 +110,8 @@ class GetNewFactHandler(AbstractRequestHandler):
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
         logger.info("In GetNewFactHandler")
+
+        
 
         random_fact = random.choice(data)
         speech = GET_FACT_MESSAGE + random_fact
